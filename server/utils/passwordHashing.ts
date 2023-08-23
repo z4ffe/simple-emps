2c9e86fa4e2ca56c67dfffc72c226efb3594d3ff
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-08-23 05:50:13 +0300

    feat: login auth on backend

---
+++ server/utils/passwordHashing.ts
@@ -0,0 +1,10 @@
+import * as bcrypt from 'bcrypt'
+
+export const hashPassword = async (password: string): Promise<string> => {
+	const SALT_ROUNDS = Number(process.env.SALT_ROUNDS) || 12
+	return await bcrypt.hash(password, SALT_ROUNDS)
+}
+
+export const verifyPassword = async (password: string, hashedPassword: string): Promise<boolean> => {
+	return await bcrypt.compare(password, hashedPassword)
+}
